87f542c3da84c9304b56857d3bdae5da97d8b91d
leonardo <web.dev.trabalho@gmail.com>
2023-02-07 09:38:27 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const port = 8080;
+
+const routes = {
+  "/": "Curso de Node.js - API Rest com Express e MongoDB.",
+  "/books": "This is the page where all the books are listed.",
+  "/authors": "This is the page where all the authors are listed.",
+  "/about":
+    "This is the page where you can find all the information about the website.",
+};
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(routes[req.url]);
+});
+
+server.listen(port, () => {
+  console.log(`Servidor escutando em http://localhost:${port}`);
+});
