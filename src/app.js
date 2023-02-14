ce16fec1b88ef55ce5d394109f50b6b58c5eb5ae
leonardo <web.dev.trabalho@gmail.com>
2023-02-14 06:38:45 -0300

    adding post method

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 import express from "express";
 
+// creating the server
+
 const app = express();
+
+// prepare the server to read json format
+
+app.use(express.json());
+
+// array of books
 
 const books = [
   { id: 1, title: "Farewell to arms" },
   { id: 2, title: "For whom the bell tolls" },
 ];
+
+// get method
 
 app.get("/", (req, res) => {
   res.status(200).send("Curso de Node.js - API Rest com Express e MongoDB");
@@ -14,5 +24,14 @@ app.get("/", (req, res) => {
 app.get("/books", (req, res) => {
   res.status(200).json(books);
 });
+
+// post method
+
+app.post("/books", (req, res) => {
+  books.push(req.body);
+  res.status(201).send("The book has been registered with success!");
+});
+
+// export the server
 
 export default app;
